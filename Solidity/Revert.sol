222d852d10cc1185a9697dafa0cd7822eb1e00f7
Sandeep-Gugulothu <152941563+Sandeep-Gugulothu@users.noreply.github.com>
2024-10-04 03:44:01 +0530

    Add files via upload

---
+++ Solidity/Revert.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+contract Require {
+    //short hand
+   address owner= msg.sender;
+   function OwnerOnly() external view{
+    //Revert if not owner
+    require(msg.sender==owner,"owneronly");
+    //do what owner says
+   }
+}
+
+//revert no state should happen 
+//no value should be transferred
+//gas is consumed
+contract Revert{
+    //@ non-privileged user attempted to access admin only method
+    error Unauthorized();
+    function adminOnly() external{
+        if(!isAdmin(msg.sender)){
+                    revert Unauthorized();
+
+        }
+    }
+}//use assert with things that should not happen
+contract Assert{
+    function withdraw() external{
+        uint balance=getBalance(msg.sender);
+        sendBalance(msg.sender);
+
+        assert(balance(msg.sender)==0);
+    }
+}
+
+
+//Example
+contract A{
+    address b;
+    constructor(address _b){
+        b=_b;
+
+    }
+        function callB() external payable{
+            (bool success, )=b.call{value:1 ether}("");
+            require(success);
+
+        }
+    
+}
+
+contract B{
+    //@thsi function should be never be paid
+    error DoNotPayMe();
+    receive() external payable{
+        revert DoNotPayMe();
+    }
+}
+
+contract exampletest{
+    A public a;
+    B public b;
+
+    function Setup() public{
+        b=new B();
+        a=new A(address(b));
+    }
+
+    function test() public{
+        a.callB{value:1.5 ether};
+        assertEq(address(a).balance,.5 ether);
+        assertEq(address(b).balance, 1 ether);
+    }
+}
